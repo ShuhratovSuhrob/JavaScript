c4bbf0c8995accf3eadb5d03d0d5b47810e11c24
Suhrob Shuhratov <shuhratovsuhrob005@gmail.com>
2024-02-07 22:28:17 +0500

    changed JS codes into script.js file

--- 05-variables/script.js
+++ 05-variables/script.js
@@ -1,1 +1,26 @@
 let cartQuantity = 0;
+
+function showCartQuantity() {
+  console.log(`Cart quantity: ${cartQuantity}`);
+}
+
+function addToCart() {
+  cartQuantity++;
+  showCartQuantity();
+}
+
+function addToCartBy2() {
+  cartQuantity += 2;
+  showCartQuantity();
+}
+
+function addToCartBy3() {
+  cartQuantity += 3;
+  showCartQuantity();
+}
+
+function resetCart() {
+  console.log('Cart was reset.'); 
+  cartQuantity = 0;
+  showCartQuantity();
+}
