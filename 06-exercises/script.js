798bfa8f1d097102ad2a25c298ed5b2dd31d88c6
Suhrob Shuhratov <shuhratovsuhrob005@gmail.com>
2024-02-09 01:56:15 +0500

    done exercise

---
+++ 06-exercises/script.js
@@ -0,0 +1,40 @@
+/*
+FIRST EXERCISE
+
+let userAge = prompt('Please enter your age: ')
+let date = prompt('Please enter todays date: ')
+
+if ((userAge < 6 || userAge >= 65) && date != 11) {
+  console.log('You have a discount in our amusement park!')
+} else if ((userAge >= 6 || userAge <= 12) && date != 11) {
+  console.log('It will be 5$')
+} else {
+  console.log('It will be 10$')
+}
+*/
+
+/*
+SECOND EXERCISE
+let flippingCoin = Math.random()
+let userGuess = prompt('Guess which side lands: ')
+ /*
+
+/*
+FIRST SOLUTON
+if (flippingCoin >= 0.5 && userGuess === 'heads') {
+  console.log('You win')
+} else {
+  console.log('You lose')
+}
+*/
+
+/*
+SECOND SOLUTION
+console.log(flippingCoin)
+
+let result =
+  flippingCoin >= 0.5 && userGuess === 'heads' ? 'You win' : 'You lose'
+
+console.log(result)
+console.log(flippingCoin)
+*/
