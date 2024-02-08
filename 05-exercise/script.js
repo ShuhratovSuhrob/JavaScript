b9cefd9f0c1f066778fb866e63864fc00e9edde9
Suhrob Shuhratov <shuhratovsuhrob005@gmail.com>
2024-02-09 01:58:37 +0500

    fixed codes

--- 05-exercise/script.js
+++ 05-exercise/script.js
@@ -1,26 +1,26 @@
-let cost = 0;
+let cost = 0
 
 function coffee() {
-  cost += 5;
-  console.log(cost);
+  cost += 5
+  console.log(cost)
 }
 
 function bagel() {
-  cost += 2;
-  console.log(cost);
+  cost += 2
+  console.log(cost)
 }
 
 function soup() {
-  cost += 9;
+  cost += 9
   console.log(cost)
 }
 
 function total() {
-  tax = 0.1;
-  cost = tax*cost + cost;
+  tax = 0.1
+  cost = tax * cost + cost
   alert(cost + '$')
 }
 
 function reOrder() {
-  cost = 0;
+  cost = 0
 }
