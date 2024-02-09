c2dd9f0e7ab6e6ad2e8565243a5c7e6a2a4162eb
Suhrob Shuhratov <shuhratovsuhrob005@gmail.com>
2024-02-10 03:24:25 +0500

    added new features

--- 07-w3schools/script.js
+++ 07-w3schools/script.js
@@ -1,3 +1,8 @@
 function getTheDate() {
   document.getElementById('demo').innerHTML = Date()
 }
+
+function getMyName() {
+  document.getElementById('myName').innerHTML =
+    'My name is Suhrob. Happy to meet you'
+}
