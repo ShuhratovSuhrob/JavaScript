b76ed3635efd3fbf1a34a6ff95b7726ef6f6e226
Suhrob Shuhratov <shuhratovsuhrob005@gmail.com>
2024-02-11 02:14:57 +0500

    added new features

--- 07-w3schools/script.js
+++ 07-w3schools/script.js
@@ -6,3 +6,19 @@ function getMyName() {
   document.getElementById('myName').innerHTML =
     'My name is Suhrob. Happy to meet you'
 }
+
+function clickToBiggerSize() {
+  document.getElementById('biggerSize').style.fontSize = '2rem'
+}
+
+function hidingParagraph() {
+  document.getElementById('changeParagraph').innerHTML = 'Second paragraph'
+}
+
+let firstNumber = parseInt(prompt('Give me the first number'))
+let secondNumber = parseInt(prompt('Give me the second number'))
+
+let sum = firstNumber + secondNumber
+function solveEquation() {
+  document.getElementById('sum').innerHTML = `the sum: ${sum}`
+}
