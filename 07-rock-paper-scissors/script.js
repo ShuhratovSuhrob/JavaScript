03d5ecc0af9e308fa74cc4b15d1ca4d4b7fcbf95
Suhrob Shuhratov <shuhratovsuhrob005@gmail.com>
2024-02-11 02:21:28 +0500

    fixed bugs

--- 07-rock-paper-scissors/script.js
+++ 07-rock-paper-scissors/script.js
@@ -1,73 +1,65 @@
 let result = ''
-let computerMove = ''
 
 function pickComputerMove() {
   const randomNumber = Math.random()
 
-  if (randomNumber >= 0 && randomNumber < 1 / 3) {
-    computerMove = 'rock'
-  } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
-    computerMove = 'paper'
-  } else if (randomNumber >= 2 / 3 && randomNumber < 1) {
-    computerMove = 'scissors'
+  if (randomNumber < 1 / 3) {
+    return 'rock'
+  } else if (randomNumber < 2 / 3) {
+    return 'paper'
+  } else {
+    return 'scissors'
   }
-
-  return computerMove
 }
 
 function playGame(playerMove) {
+  const computerMove = pickComputerMove()
+
   if (playerMove === 'rock') {
-    const rockBtn = document.querySelector('#rock')
-    rockBtn.addEventListener('click', function () {
-      let computerMove = pickComputerMove()
-
-      if (computerMove == 'rock') {
-        result = 'Tie'
-      } else if (computerMove == 'paper') {
-        result = 'Computer wins'
-      } else if (computerMove == 'scissors') {
-        result = 'You win'
-      }
-
-      displayWinner(computerMove, result)
-    })
+    if (computerMove === 'rock') {
+      result = 'Tie'
+    } else if (computerMove === 'paper') {
+      result = 'Computer wins'
+    } else if (computerMove === 'scissors') {
+      result = 'You win'
+    }
   }
 
   if (playerMove === 'paper') {
-    const paperBtn = document.querySelector('#paper')
-    paperBtn.addEventListener('click', function () {
-      let computerMove = pickComputerMove()
-
-      if (computerMove == 'paper') {
-        result = 'Tie'
-      } else if (computerMove == 'rock') {
-        result = 'You win'
-      } else if (computerMove == 'scissors') {
-        result = 'Computer wins'
-      }
-
-      displayWinner(computerMove, result)
-    })
+    if (computerMove === 'paper') {
+      result = 'Tie'
+    } else if (computerMove === 'rock') {
+      result = 'You win'
+    } else if (computerMove === 'scissors') {
+      result = 'Computer wins'
+    }
   }
 
   if (playerMove === 'scissors') {
-    const sciBtn = document.querySelector('#sci')
-    sciBtn.addEventListener('click', function () {
-      const computerMove = pickComputerMove()
-
-      if (computerMove === 'scissors') {
-        result = 'Tie'
-      } else if (computerMove === 'rock') {
-        result = 'Computer wins'
-      } else if (computerMove === 'paper') {
-        result = 'You win'
-      }
-
-      displayWinner(computerMove, result, playerMove)
-    })
+    if (computerMove === 'scissors') {
+      result = 'Tie'
+    } else if (computerMove === 'rock') {
+      result = 'Computer wins'
+    } else if (computerMove === 'paper') {
+      result = 'You win'
+    }
   }
+
+  displayWinner(computerMove, result)
 }
 
 function displayWinner(computerMove, result) {
   alert(`You picked scissors. Computer picked ${computerMove}. ${result}`)
 }
+
+document.querySelector('#rock').addEventListener('click', function () {
+  playGame('rock')
+})
+
+document.querySelector('#paper').addEventListener('click', function () {
+  playGame('paper')
+})
+
+document.querySelector('#sci').addEventListener('click', function () {
+  playGame('scissors')
+})
